a48980d5abdfb4480516dabcc9a135366a9e00dc
Cyril GIACOPINO <cgiacopino@ymagyn.com>
2014-04-22 11:44:00 +0200

    Update task

--- tasks/yaml_to_swagger.js
+++ tasks/yaml_to_swagger.js
@@ -117,6 +117,7 @@ module.exports = function (grunt) {
                 }
             } else {
                 clearInterval(interval);
+                callback();
             }
         }, 100);
     }
@@ -132,15 +133,11 @@ module.exports = function (grunt) {
                     var args = ['schema', require("path").resolve(options.models_path + '/' + models[0] + '.ts')];
                     execSync('typson', args, function (error, result, code) {
                         if (error == null) {
-                            var pretty_schema = JSON.stringify(JSON.parse(result), undefined, 2);
-                            fs.writeFileSync(options.models_path + 'schema/' + models[0] + '.json', pretty_schema);
-
                             var result = JSON.stringify(JSON.parse(result));
                             var pattern = new RegExp('\[MockType=(\w)+\]', "g");
                             var matches = result.match(/\[MockType=(\w)+\]/g);
-                            if (matches!=null &&matches.length > 0) {
-                                for(var replace=0;replace<matches.length;replace++)
-                                {
+                            if (matches != null && matches.length > 0) {
+                                for (var replace = 0; replace < matches.length; replace++) {
                                     result = result.split(matches[replace]).join("");
                                 }
                             }
@@ -163,27 +160,70 @@ module.exports = function (grunt) {
 
     }
 
+    function createMockSchema(options, callback) {
+        var fs = require('fs');
+
+        var models_files = fs.readdirSync(options.models_path);
+        var models = [];
+        for (var i = 0; i < models_files.length; i++) {
+            var file = models_files[i];
+            var extension = file.split('.').pop();
+            if (extension == "ts") {
+                models.push(file);
+            }
+
+        }
+        var returnData = {};
+        var working = false;
+        var interval = setInterval(function () {
+            if (models.length > 0) {
+                if (working == false) {
+                    working = true;
+                    var args = ['schema', require("path").resolve(options.models_path + '/' + models[0])];
+                    execSync('typson', args, function (error, result, code) {
+                        if (error == null) {
+                            var pretty_schema = JSON.stringify(JSON.parse(result), undefined, 2);
+                            fs.writeFileSync(options.models_path + 'schema/' + models[0].split(".ts").join('.json'), pretty_schema);
+                            models.splice(0, 1);
+                            working = false;
+                        } else {
+                            grunt.log.error('typson ' + args.join(" "));
+                            grunt.fatal(error);
+                        }
+
+                    });
+
+                }
+            } else {
+                clearInterval(interval);
+                callback();
+            }
+        }, 100);
+    }
+
     grunt.registerMultiTask('yaml_to_swagger', 'Convert YAML files into swagger compatible JSON Schema format', function () {
 
         var fs = require('fs');
         var options = this.options();
 
         var done = this.async();
-
-        var path_api = require('path').resolve(options.route_path + '/api.yml');
-        var api_definitions = require('yamljs').load(path_api);
-        var files = [];
-        for (var i = 0; i < api_definitions.apis.length; i++) {
-            var path = api_definitions.apis[i].path.split("/").join("");
-            files.push(api_definitions.apis[i].path + '.yml');
-        }
-        var outputFilename = options.output_docs_path + "/api.json";
-        var pretty_route_definitions = JSON.stringify(api_definitions, undefined, 2);
-        fs.writeFileSync(outputFilename, pretty_route_definitions);
-        grunt.log.ok("/api.json created");
-        parseFiles(files, options, function () {
-            done();
+        createMockSchema(options, function () {
+            var path_api = require('path').resolve(options.route_path + '/api.yml');
+            var api_definitions = require('yamljs').load(path_api);
+            var files = [];
+            for (var i = 0; i < api_definitions.apis.length; i++) {
+                var path = api_definitions.apis[i].path.split("/").join("");
+                files.push(api_definitions.apis[i].path + '.yml');
+            }
+            var outputFilename = options.output_docs_path + "/api.json";
+            var pretty_route_definitions = JSON.stringify(api_definitions, undefined, 2);
+            fs.writeFileSync(outputFilename, pretty_route_definitions);
+            grunt.log.ok("/api.json created");
+            parseFiles(files, options, function () {
+                done();
+            });
         });
+
     });
 
 };
